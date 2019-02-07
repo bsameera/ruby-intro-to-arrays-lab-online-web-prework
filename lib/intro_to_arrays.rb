6b1334e61e7a0ba7d9eff7830ed7aa685a8de06c
splendid-frame-7429 <bsameera@yahoo.com>
2019-02-07 06:32:15 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,3 +7,15 @@ def array_with_two_elements
   arr = [5, 7]
   arr
 end
+
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(arr)
+  arr[-1]
+end
